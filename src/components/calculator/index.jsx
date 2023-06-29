5c41e2695c5c4556818366fe7637fd1259060a8b
Eloy <eloymjunior00@gmail.com>
2023-06-29 09:57:54 -0300

    {Feat}(Calc):Envio dos inputs para o novo Template do PandaDoc

--- src/components/calculator/index.jsx
+++ src/components/calculator/index.jsx
@@ -1,55 +1,8 @@
-import React, { useState } from "react";
-import Form0 from "./Step0";
-import Form1 from "./Step1";
-// import Form2 from "./Step2";
-// import Form3 from "./Step3";
-// import Form4 from "./Step4";
+import React from "react";
 import StepTypeOfEntitie from "./StepCalc";
 
 export default function StepForms() {
-  const [step, setStep] = useState(1);
-
-  const handleNextStep = () => {
-    setStep(step + 1);
-  };
-
-  const handlePreviousStep = () => {
-    setStep(step - 1);
-  };
-
   return (
-    <>
-      {step === 1 && <Form0 onNextStep={handleNextStep} />}
-      {/* {step === 2 && (
-        <Form1
-          onNextStep={handleNextStep}
-          onPreviousStep={handlePreviousStep}
-        />
-      )} */}
-      {/* {step === 3 && (
-        <Form2
-          onNextStep={handleNextStep}
-          onPreviousStep={handlePreviousStep}
-        />
-      )}
-      {step === 4 && (
-        <Form3
-          onNextStep={handleNextStep}
-          onPreviousStep={handlePreviousStep}
-        />
-      )}
-      {step === 5 && (
-        <Form4
-          onNextStep={handleNextStep}
-          onPreviousStep={handlePreviousStep}
-        />
-      )} */}
-      {step === 2 && (
-        <StepTypeOfEntitie
-          onNextStep={handleNextStep}
-          onPreviousStep={handlePreviousStep}
-        />
-      )}
-    </>
+    <StepTypeOfEntitie />
   );
 }
