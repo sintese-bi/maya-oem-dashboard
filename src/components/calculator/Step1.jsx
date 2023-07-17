dc80241ebee64942d714f95fc5121c3dd7e8113c
Eloy <eloymjunior00@gmail.com>
2023-07-17 09:34:08 -0300

    {Feat}(Calc):Adicionando comentarios

--- src/components/calculator/Step1.jsx
+++ src/components/calculator/Step1.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
-//Home page da Calculadora
+//Home page da Calculadora.
 function HomePage() {
   return (
     <div
