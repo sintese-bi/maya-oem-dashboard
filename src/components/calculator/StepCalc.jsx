44dddfa24f36f1117636963f09b9bb5e4bff3d16
Eloy <eloymjunior00@gmail.com>
2023-07-18 17:13:23 -0300

    {Fix!}(Calc):Alterações gerais2

--- src/components/calculator/StepCalc.jsx
+++ src/components/calculator/StepCalc.jsx
@@ -142,7 +142,7 @@ export default function StepTypeOfEntitie({ onPreviousStep }) {
   const fetchRadiacao = async () => {
     try {
       const response = await axios.get(
-        `https://app.mayaoem.com.br/v1/irrcoef/${encodeURIComponent(cidade)}`
+        `${process.env.REACT_APP_BASE_URL}/v1/irrcoef/${encodeURIComponent(cidade)}`
       );
       const data = response.data;
       if (data && data.ic_yearly) {
@@ -200,7 +200,7 @@ export default function StepTypeOfEntitie({ onPreviousStep }) {
         clientGiga = apiResponse.month;
       }
       const response = await axios.post(
-        `https://app.mayaoem.com.br/v1/pandadoc`,
+        `${process.env.REACT_APP_BASE_URL}/v1/pandadoc`,
         {
           clientPot: potenciaModulos,
           clientEstimated: valorEstimado,
