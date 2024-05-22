67f9775dd06edb0ffff37e312d4cb9301276fd2d
felipe-gl18 <felipegadelha2004@gmail.com>
2024-05-22 14:49:29 -0300

    adds

--- src/components/mobile-test/mobile-test.jsx
+++ src/components/mobile-test/mobile-test.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import MayaXBlueLogo from "src/assets/img/logo/maya-x-blue.png";
 import { MobileDados } from "./mobile-test-dados/mobile-test-dados";
 
-
 export const MobileTest = () => {
   return (
     <Box
