a6b660a4e7438c2bdde2b81d152cdbb97a2ceae7
felipe-gl18 <felipegadelha2004@gmail.com>
2024-05-27 17:28:42 -0300

    adds

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { getUserCookie } from "./session";
 
 // CRIAÇÃO DA INSTANCIA DO AXIOS
 const api = axios.create({
-  url: `https://app.mayaoem.com.br/v1` + url,
+  url: `https://app.mayaoem.com.br/v1`,
   //baseURL: `http://localhost:8081/v1`,
 });
 
